dc7c7c37fc5509145cdd6eaaebfdf1cbb83c0b06
Ioannis Kapritsias <jkapritsias@gmail.com>
2017-04-21 16:34:23 +0200

    dotenv actually working

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
 const path = require('path');
 const router = require('./routes/router');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv').config();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -13,11 +13,17 @@ app.use(express.static('public'));
 
 const Twitter = require('twitter');
 
+
+require('dotenv').config();
+
+const testUrl = process.env.TESTURL;
+const urlSingle = process.env.URLSINGLE;
+
 //credentials twitter api
-const twitterConsumerKey = "sApa7Tgmj2qwFhTtYVOufHo96";
-const twitterConsumerSecret = "RNRaFY1LEUTgT8Y9PF1oHmuHN5Ugih15aHxn3Gdb0pmF8HakPJ";
-const access_token = '2214014072-1LTEGnQIJkozwh39dZwxewvCg7WyixoqbUubl9o';
-const access_token_secret = 'yOem5zbUCJNMffBXNSNVTpPD6hrbsYKC2tLl6nNoieTZq';
+twitterConsumerKey = process.env.twitterConsumerKey;
+twitterConsumerSecret = process.env.twitterConsumerSecret;
+accesstoken = process.env.accesstoken;
+accesstokensecret = process.env.accesstokensecret;
 
 //oauth setup
 
@@ -28,8 +34,8 @@ const access_token_secret = 'yOem5zbUCJNMffBXNSNVTpPD6hrbsYKC2tLl6nNoieTZq';
 const client = new Twitter({
     consumer_key: twitterConsumerKey,
     consumer_secret: twitterConsumerSecret,
-    access_token_key: access_token,
-    access_token_secret: access_token_secret
+    access_token_key: accesstoken,
+    access_token_secret: accesstokensecret
 });
 
 
