ad3776b2a1bd50417a9d25ab56d7cf800013995c
Ioannis Kapritsias <jkapritsias@gmail.com>
2017-04-21 15:58:01 +0200

    readme setup

--- routes/router.js
+++ routes/router.js
@@ -6,21 +6,4 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
-// router.get('/oauth', funciton(request, response) {
-//   rp({
-//     uri: '',
-//     qs: {
-//       client_id:
-//       client_secret:
-//       code:
-//       redirect_uri:
-//     }
-//   })
-//   .then(function (data) {
-//     data = JSON.parse(data);
-//     request.app.accessToken = data.acces_token;
-//     res.redirect(oauth);
-//   });
-// });
-
 module.exports = router;
