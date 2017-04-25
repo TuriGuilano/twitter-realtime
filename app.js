ea44f3f3a383b80a72a7e5bbf89f67b69a152aad
Ioannis Kapritsias <jkapritsias@gmail.com>
2017-04-25 11:16:04 +0200

    comments / deleted spaces and logs

--- app.js
+++ app.js
@@ -5,31 +5,21 @@ const io = require('socket.io').listen(server);
 const path = require('path');
 const router = require('./routes/router');
 const dotenv = require('dotenv').config();
+const Twitter = require('twitter');
+
+const testUrl = process.env.TESTURL;
+const urlSingle = process.env.URLSINGLE;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use('/', router);
 app.use(express.static('public'));
 
-const Twitter = require('twitter');
-
-
-require('dotenv').config();
-
-const testUrl = process.env.TESTURL;
-const urlSingle = process.env.URLSINGLE;
-
 //credentials twitter api
 twitterConsumerKey = process.env.twitterConsumerKey;
 twitterConsumerSecret = process.env.twitterConsumerSecret;
 accesstoken = process.env.accesstoken;
 accesstokensecret = process.env.accesstokensecret;
-
-//oauth setup
-
-// const oauth_token = '';
-// const oauth = 'https://api.twitter.com/oauth/authorize?oauth_token=${oauth_token}';
-
 
 const client = new Twitter({
     consumer_key: twitterConsumerKey,
@@ -38,26 +28,18 @@ const client = new Twitter({
     access_token_secret: accesstokensecret
 });
 
-
 io.sockets.on('connection', function(socket){
   socket.on('userQuery', function(value) {
-    console.log('eerste log', value);
-
     client.stream('statuses/filter', {track: value},  function(stream) {
        stream.on('data', function(tweet) {
            io.emit('tweet', tweet);
-           console.log('tweede log', value);
        });
-
        stream.on('error', function(error) {
            console.log(error);
        });
     });
-
   })
 });
 
-
-
 server.listen(process.env.PORT || 8888);
-console.log('server is running on');
+console.log('server is running on port 8888');
--- public/js/client.js
+++ public/js/client.js
@@ -1,27 +1,35 @@
+//iffy
 (function(){
+    //define socket.io
     const socket = io.connect();
-
+    //function that listens to the the user and gets the input / button event
     function startProcess() {
+
       const userInput = document.getElementById('search');
       const triggerSome = document.getElementById('fireEvent');
+
       triggerSome.addEventListener('click', function() {
         event.preventDefault();
+
         console.log('val van de input', userInput.value);
         const userQuery = userInput.value;
-
+        // emit userQuery to server
         socket.emit('userQuery', userQuery);
 
       });
     }
 
     startProcess();
-
+    
+    //append tweets to body
     socket.on('tweet', function (tweet) {
+
       const tweetObject = tweet.text;
       const tweetInfo = document.createElement('div');
 
       tweetInfo.classList.add('tweet-specifics');
       tweetInfo.innerHTML = tweetObject;
+
       document.body.appendChild(tweetInfo);
     });
 })();
